b0bcc5f1b18a6cd645a1d475fc441d6ca62cb48f
Daniel Muhoro <hellodannymuhoro@gmail.com>
2026-03-26 23:15:05 +0300

    Fix: Enable require.context and add expo-router/babel

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,7 @@ const { withNativeWind } = require("nativewind/metro");
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Enable require.context
+config.transformer.unstable_allowRequireContext = true;
+
 module.exports = withNativeWind(config, { input: "./global.css" });
